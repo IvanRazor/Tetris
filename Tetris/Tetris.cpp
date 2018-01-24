bc80ebb1130cf834ab15f71598cabc0c27da3491
Ivan Kukushkin <mugenkiv@gmail.com>
2018-01-24 23:21:07 +0100

    SFML libs and settings

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+/*
+#include <sfml/audio.hpp> 
+#include <sfml/config.hpp> 
+#include <sfml/network.hpp> 
+#include <sfml/opengl.hpp> 
+#include <sfml/system.hpp> 
+#include <sfml/window.hpp>*/
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
     return 0;
 }
 
